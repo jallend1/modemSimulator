780319927b131124428fd084693c9e46b1cfe22d
Jason Allen <39307371+jallend1@users.noreply.github.com>
2023-03-12 17:31:23 -0700

    Displays sample text at 14.4k

---
+++ sampleText.js
@@ -0,0 +1,4 @@
+// Creates a variable with 5000 characters of text
+
+const text =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl';
